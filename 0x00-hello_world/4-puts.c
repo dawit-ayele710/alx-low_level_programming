360039a931af9b6c84506396a527b4d3631760aa
Dawit Ayele <dawitayel710@gmail.com>
2023-02-18 09:41:22 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *main - Entry point
- *return: always (0) (Success)
-**/
+ * main - A program that prints a line with puts function
+ * return: always (0) (Success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

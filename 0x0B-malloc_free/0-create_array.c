cb4d6b230343123c170cb0874868176f1407dc3e
Dawit Ayele <dawitayel710@gmail.com>
2023-03-15 14:28:00 -0700

    0. Float like a butterfly, sting like a bee3

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,9 +6,9 @@
  * @size: size of array
  * @c: char to assign
  * Description: creat array of size size and assign char c
- * Return: pointer to array, NULL if fail	
+ * Return: pointer to array, NULL if fail
  */
-char *create_array(unsigned int size, char c)		
+char *create_array(unsigned int size, char c)
 {
 	char *str;
 	unsigned int i;
@@ -17,5 +17,5 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		str[i] = c;
-	return (str);		
+	return (str);
 }

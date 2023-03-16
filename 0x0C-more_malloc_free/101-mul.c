cfcda9f8caaa5a3cf811a66035a9c94c3ead0acf
Dawit Ayele <dawitayel710@gmail.com>
2023-03-16 13:00:25 -0700

    0x0C. C - More malloc, free3

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -10,7 +10,7 @@
 int is_digit(char *s)
 {
 	int i = 0;
-	
+
 	while (s[i])
 	{
 		if (s[i] < '0' || s[i] > '9')
@@ -27,7 +27,7 @@ int is_digit(char *s)
 int _strlen(char *s)
 {
 	int i = 0;
-	
+
 	while (s[i] != '\0')
 	{
 		i++;
@@ -52,7 +52,7 @@ int main(int argc, char *argv[])
 {
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
-	
+
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
